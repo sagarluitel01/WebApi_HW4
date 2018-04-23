487d16ec259b1ce7eb805e165c83a0a0e7fd472c
5a9ar <sagarluitel01>
2018-04-22 22:48:41 -0600

    007

--- Movie.js
+++ Movie.js
@@ -3,17 +3,22 @@ var Schema = mongoose.Schema;
 
 mongoose.connect('mongodb://sagar01:D3goldhap@ds111535.mlab.com:11535/webapidatabase');
 
-var Actors = new Schema({
+/*var Actors = new Schema({
     actorName: {type: String, required: true},
     characterName: {type: String, required: true},
-})
+})*/
 
 var MovieSchema = new Schema({
     Title: String,
     Year: String,
     Genre: [ "Action", "Adventure", "Comedy", "Drama", "Fantasy", "Horror", "Mystery", "Thriller",
         "Western"],
-    Actors : {type: [Actors]}
+    Actors: [
+        {
+            ActorName: String,
+            CharacterName: String
+        }
+    ]
 });
 
 MovieSchema.pre('save', function(next) {
