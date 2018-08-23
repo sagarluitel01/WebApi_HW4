1a276805ab0e626b27e7a70c2303a5597f2c9bca
5a9ar <sagar_luitel25@yahoo.com>
2018-08-23 10:10:43 -0600

    Update Review.js

--- Review.js
+++ Review.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://sagar01:D3goldhap@ds111535.mlab.com:11535/webapidatabase');
+mongoose.connect('mongodb://username:Password@ds111535.mlab.com:11535/webapidatabase');
 
 var ReviewSchema = new Schema({
     Movie: { type: String, required: true },
