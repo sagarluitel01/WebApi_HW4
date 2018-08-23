16909791475d971678fdfe1a8ffcdf6405ab2463
5a9ar <sagar_luitel25@yahoo.com>
2018-08-23 10:11:21 -0600

    Update Movie.js

--- Movie.js
+++ Movie.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://sagar01:D3goldhap@ds111535.mlab.com:11535/webapidatabase');
+mongoose.connect('mongodb://username:password@ds111535.mlab.com:11535/webapidatabase');
 
 var actor = new Schema({
     ActorName: {type: String, required: true},
